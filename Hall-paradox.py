d8f277800f9eb27bfd21484add984e8aa1921add
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-05-12 22:26:25 +0300

    Причесал код

--- Hall-paradox.py
+++ Hall-paradox.py
@@ -7,23 +7,11 @@ def proof(iterations,door_amount=3):
         Secomn - number of wins with door change.
     '''
     from random import randint
-    win1 = 0
-    win2 = 0
+    win1,win2 = 0,0
     for i in range(iterations):## x > 0:
-        a = randint(1,door_amount)
-        b = randint(1,door_amount)
-        c = randint(1,door_amount)
-        while c == b:
-            c = randint(1,door_amount)
-        if a == b:
-            win1 +=1
-        if a == b or a == c:
-            win2 +=1
-    print(
-        'Without cange: ',
-        (win1/iterations)*100,
-        '%\nWith change: ',
-        (win2/iterations)*100,'%'
-        )
+        a,b,c = randint(1,door_amount),randint(1,door_amount),randint(1,door_amount)
+        while c == b: c = randint(1,door_amount)
+        if a == b: win1 +=1
+        if a == b or a == c: win2 +=1
+    print('Without cange: {}%\nWith change: {}%'.format((win1/iterations)*100,(win2/iterations)*100))
     return win1,win2
-
